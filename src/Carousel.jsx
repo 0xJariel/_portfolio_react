0eb69e1962956e0550be34a0116bbae75fd79b5a
J <0xjariel@protonmail.com>
2023-07-31 00:42:05 -0600

    page 2 carousel functionality complete

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,34 +1,99 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { styled } from "styled-components";
-import { useState } from "react";
-
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 export default function Carousel() {
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const carouselRefs = [useRef(null), useRef(null), useRef(null)];
 
-  const carouselData = [
-    { id: 1, color: "pink" },
-    { id: 2, color: "yellow" },
-    { id: 3, color: "lightblue" },
-  ];
+  const [carousel, setCarousel] = useState([
+    { id: 1, position: 1, color: "pink" },
+    { id: 2, position: 2, color: "yellow" },
+    { id: 3, position: 3, color: "lightblue" },
+  ]);
 
-  const rotateClockWise = () => {};
+  const rotateClockWise = () => {
+    carouselRefs.forEach((box) => {
+      //! can't access custom dom properties theough current
+      const position = box.current.getAttribute("position");
+      switch (true) {
+        case position == 0:
+          console.log(box);
+          box.current.style.transform =
+            "translateX(0px) perspective(100px) translateZ(25px)";
+          box.current.style.zIndex = "101";
+          box.current.setAttribute("position", "1");
+          break;
+        case position == 1:
+          box.current.style.transform =
+            "translateX(50%) perspective(100px) translateZ(0)";
+          box.current.style.zIndex = "50";
+          box.current.setAttribute("position", "2");
+          break;
+        case position == 2:
+          box.current.style.transform = "translateX(-50%)";
+          box.current.setAttribute("position", "0");
+          break;
+      }
+    });
+  };
+
+  const rotateCounterClockwise = () => {
+    carouselRefs.forEach((box) => {
+      //! can't access custom dom properties theough current
+      const position = box.current.getAttribute("position");
+      switch (true) {
+        case position == 0:
+          box.current.style.zIndex = "25";
+          box.current.style.transform =
+            "translateX(50%) perspective(100px) translateZ(0)";
+          box.current.setAttribute("position", "2");
+
+          break;
+        case position == 1:
+          box.current.style.zIndex = "50";
+          console.log(box);
+          box.current.style.transform = "translateX(-50%)";
+          box.current.setAttribute("position", "0");
+
+          break;
+        case position == 2:
+          box.current.style.zIndex = "101";
+          box.current.style.transform =
+            "translateX(0px) perspective(100px) translateZ(25px)";
+          box.current.setAttribute("position", "1");
+          break;
+      }
+    });
+  };
 
   return (
     <StyledCarousel>
-      <div className="previous">previous</div>
+      <div
+        className="previous"
+        onClick={() => {
+          rotateCounterClockwise();
+        }}
+      >
+        <BsArrowLeftCircleFill />
+      </div>
 
       <div className="box-container">
-        {carouselData.map((box) => (
-          <div key={box.id} className="box"></div>
+        {carousel.map((box, index) => (
+          <div
+            key={box.id}
+            className="box"
+            style={{ backgroundColor: box.color }}
+            ref={carouselRefs[index]}
+            position={index}
+          ></div>
         ))}
       </div>
       <div
         className="next"
         onClick={() => {
-          rotateClockwise();
+          rotateClockWise();
         }}
       >
-        next
+        <BsArrowRightCircleFill />
       </div>
     </StyledCarousel>
   );
@@ -37,22 +102,24 @@ export default function Carousel() {
 const StyledCarousel = styled.div`
   display: grid;
   align-items: center;
+  /* justify-items: center; */
 
   padding: 0 var(--padding-sides);
   grid-template-columns: auto auto auto;
-  gap: 15px;
+  /* gap: 15px; */
 
   .box-container {
-    min-width: 450px;
+    min-width: 400px;
     min-height: 170px;
     display: grid;
-    grid-template-columns: repeat(3, minmax(auto, 290px));
-    justify-content: center;
-    align-items: center;
+    justify-items: center;
+
+    /* grid-template-columns: minmax(auto, 290px); */
   }
 
   .box {
     content: "";
+    position: absolute;
     width: 100%;
     height: 100%;
     max-width: 290px;
@@ -63,18 +130,18 @@ const StyledCarousel = styled.div`
   }
 
   .box-container :nth-child(1) {
-    z-index: 75;
-    transform: translateX(25%);
+    z-index: 100;
+    background-color: tan;
+    transform: translatex(-50%);
   }
 
   .box-container :nth-child(2) {
-    z-index: 100;
+    z-index: 101;
     transform: perspective(100px) translateZ(25px);
   }
 
   .box-container :nth-child(3) {
-    z-index: 50;
-    transform: translateX(-25%);
+    transform: translatex(50%);
   }
 
   #box1 {
@@ -87,18 +154,27 @@ const StyledCarousel = styled.div`
     background-color: lightblue;
   }
 
+  .previous {
+    justify-self: right;
+  }
+
+  .previous,
+  .next {
+    font-size: 48px;
+  }
+
   @media (max-width: 800px) {
     /* change carousel to something modile */
   }
 `;
 
-const rotateClockwise = () => {
-  console.log("rotate clockwise!");
-  const box1 = document.querySelector(".box-container :nth-child(1) ");
-  const box2 = document.querySelector(".box-container :nth-child(2)");
-  const box3 = document.querySelector(".box-container :nth-child(3)");
+// const rotateClockwise = () => {
+//   console.log("rotate clockwise!");
+//   const box1 = document.querySelector(".box-container :nth-child(1) ");
+//   const box2 = document.querySelector(".box-container :nth-child(2)");
+//   const box3 = document.querySelector(".box-container :nth-child(3)");
 
-  box1.style.transform = "translateX(20%) ";
+//   box1.style.transform = "translateX(20%) ";
 
-  box2.style.transform = "translateX(100%) ";
-};
+//   box2.style.transform = "translateX(100%) ";
+// };
